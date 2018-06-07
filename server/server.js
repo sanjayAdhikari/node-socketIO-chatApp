5dbb4a244a30faa81ebb3ef763d69726601150b1
sanjayAdhikari <idsanjay81@gmail.com>
2018-06-07 23:20:44 +0545

    emit and listen socketIO

--- server/server.js
+++ server/server.js
@@ -14,9 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
     console.log('new User is connected');
 
+    socket.on('createMessage', (data) => {
+        console.log(data);
+    })
+
     socket.on('disconnect', () => {
         console.log('User is disconnected');
     })
+
+    socket.emit('newMessage', {
+        from: 'Dipesh Dulal',
+        text: 'I am good ! Sanjay'
+    })
+
 })
 server.listen(port, () => {
     console.log(`Server is up ${port} port`);
